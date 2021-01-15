14861c37316d84bd1c386ee6dd92673b9b20cbfc
seaboyz <seaboyz@hotmail.com>
2021-01-15 13:33:41 -0600

    Add starter files

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const newButton = document.querySelector(".btn--new");
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+const diceImage = document.querySelector(".dice");
+const rollDiceButton = document.querySelector(".btn-roll");
+const holdButton = document.getElementsByClassName(".btn--hold");
+
+diceImage.classList.add("hidden");
